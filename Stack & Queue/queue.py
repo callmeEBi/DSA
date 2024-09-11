24de52364821cb17c201d53ba0f4af3deeb0219c
EBi <ebi.x2003x@gmail.com>
2024-09-11 23:40:12 +0330

    added a condition to enqueue method

--- Stack & Queue/queue.py
+++ Stack & Queue/queue.py
@@ -19,8 +19,12 @@ class Queue:
 
     def enqueue(self, value):
         new_node = Node(value)
-        self.last.next = new_node
-        self.last = new_node
+        if self.length == 0:
+            self.last = new_node
+            self.first = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
         self.length += 1
 
 
