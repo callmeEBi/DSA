4a12cea381e944b8c84d69b3363869555c70d667
EBi <ebi.x2003x@gmail.com>
2024-09-11 13:24:14 +0330

    add shift method

--- Doubly Linked List/DLL.py
+++ Doubly Linked List/DLL.py
@@ -1,4 +1,5 @@
 class Node:
+
     def __init__(self, value):
         self.value = value
         self.next = None
@@ -6,6 +7,7 @@ class Node:
 
 
 class DoublyLinkedList:
+
     def __init__(self, value) -> None:
         new_node = Node(value)
         self.head = new_node
@@ -14,6 +16,7 @@ class DoublyLinkedList:
 
     def print_list(self):
         temp = self.head
+
         while temp:
             print(temp.value)
             temp = temp.next
@@ -24,6 +27,7 @@ class DoublyLinkedList:
         if not self.length:
             self.head = new_node
             self.tail = new_node
+
         else:
             self.tail.next = new_node
             new_node.prev = self.tail
@@ -32,14 +36,17 @@ class DoublyLinkedList:
         return True
 
     def pop(self):
+
         if not self.length:
             return None
         last_node = self.tail
+
         if self.length == 1:
             self.head = None
             self.tail = None
             last_node.prev = None
             last_node.next = None
+
         else:
             self.tail = self.tail.prev
             last_node.prev = None
@@ -49,20 +56,44 @@ class DoublyLinkedList:
 
     def prepend(self, value):
         new_node = Node(value)
+
         if not self.length:
             self.head = new_node
             self.tail = new_node
+
         else:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
         self.length += 1
 
+    def shift(self):
+
+        if not self.length:
+            return None
+
+        if self.length == 1:
+            return self.pop()
+        first_node = self.head
+        self.head = self.head.next
+        self.head.prev = None
+        first_node.next = None
+        self.length -= 1
+        return first_node
+
 
 my_dll = DoublyLinkedList(27)
-my_dll.prepend(95)
+my_dll.append(46)
+my_dll.append(70)
+my_dll.append(77)
+
+my_dll.shift()
+my_dll.shift()
+my_dll.shift()
+my_dll.shift()
 
 my_dll.print_list()
+
 
 # FIXME - always do this in new method:
 # consider length
