39714c1f96d1d22ae4fe12f035c2a699db9e1532
EBi <ebi.x2003x@gmail.com>
2024-09-11 13:16:43 +0330

    create prepend method

--- Doubly Linked List/DLL.py
+++ Doubly Linked List/DLL.py
@@ -32,14 +32,14 @@ class DoublyLinkedList:
         return True
 
     def pop(self):
+        if not self.length:
+            return None
         last_node = self.tail
         if self.length == 1:
             self.head = None
             self.tail = None
             last_node.prev = None
             last_node.next = None
-        elif not self.length:
-            return None
         else:
             self.tail = self.tail.prev
             last_node.prev = None
@@ -47,11 +47,22 @@ class DoublyLinkedList:
         self.length -= 1
         return last_node
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if not self.length:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
 
-my_dll = DoublyLinkedList(7)
-my_dll.pop()
-my_dll.pop()
-# my_dll.print_list()
+
+my_dll = DoublyLinkedList(27)
+my_dll.prepend(95)
+
+my_dll.print_list()
 
 # FIXME - always do this in new method:
 # consider length
