c244f4dfc7d97e6f3ebfe9154fc01689df3a4ec7
EBi <ebi.x2003x@gmail.com>
2024-09-11 22:48:36 +0330

    added pop method

--- Stack & Queue/stack.py
+++ Stack & Queue/stack.py
@@ -22,15 +22,23 @@ class Stack:
         self.top = new_node
         self.height += 1
 
+    def pop(self):
+        if self.height == 0:
+            return None
+        last_node = self.top
+        self.top = self.top.next
+        last_node.next = None
+        self.height -= 1
+        return last_node
+
 
 my_stack = Stack(9)
-my_stack.push(83)
-my_stack.push(80)
-my_stack.push(14)
+my_stack.pop()
+my_stack.push(88)
 my_stack.print_stack()
 
 
 # FIXME - always in a new method:
-# consider length
+# consider height
 # consider situation with one item in the list
 # consider situation with empty list
