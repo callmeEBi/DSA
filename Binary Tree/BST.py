d229243d2ee4d38cbc8018868ab959d40e3f0b5e
EBi <ebi.x2003x@gmail.com>
2024-09-12 13:17:49 +0330

    create "contains" method

--- Binary Tree/BST.py
+++ Binary Tree/BST.py
@@ -1,5 +1,4 @@
 class Node:
-
     def __init__(self, value) -> None:
         self.value = value
         self.right = None
@@ -7,52 +6,53 @@ class Node:
 
 
 class BinarySearchTree:
-
     def __init__(self) -> None:
         self.root = None
 
     def insert(self, value):
         new_node = Node(value)
-
         if not self.root:
             self.root = new_node
             return True
         compared_node = self.root
-
         while True:
-
             if value == compared_node.value:
                 return False
-
             if value > compared_node.value:
-
                 if compared_node.right:
                     compared_node = compared_node.right
-
                 else:
                     compared_node.right = new_node
                     return True
-
             else:
-
                 if compared_node.left:
                     compared_node = compared_node.left
-
                 else:
                     compared_node.left = new_node
                     return True
 
+    def contains(self, value):
+        comparing = self.root
+        while comparing:
+            if value == comparing.value:
+                return True
+            if value > comparing.value:
+                comparing = comparing.right
+            else:
+                comparing = comparing.left
+        return False
+
 
 my_bst = BinarySearchTree()
-my_bst.insert(47)
-my_bst.insert(47)
-my_bst.insert(76)
-my_bst.insert(18)
-my_bst.insert(52)
-my_bst.insert(82)
+my_bst.insert(67)
+my_bst.insert(97)
+my_bst.insert(11)
+my_bst.insert(17)
+my_bst.insert(63)
+my_bst.insert(75)
+my_bst.insert(67)
+my_bst.insert(30)
+my_bst.insert(22)
+my_bst.insert(15)
 
-print(my_bst.root.value)
-print(my_bst.root.right.value)
-print(my_bst.root.left.value)
-print(my_bst.root.right.right.value)
-print(my_bst.root.right.left.value)
+print(my_bst.contains(888))
