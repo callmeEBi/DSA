37bbe56e7b3b7dc3853212f44b337fff0954f97e
EBi <EBi.x2003x@gmail.com>
2024-09-16 17:44:27 +0330

    "Simplified swap operation in bubble_sort function and changed function call to modify original list."

--- Sorts/bubble_sort.py
+++ Sorts/bubble_sort.py
@@ -9,12 +9,10 @@ def bubble_sort(items_list):
     while not is_sorted(items_list):
         for i in range(len(items_list) - 1):
             if items_list[i] > items_list[i + 1]:
-                temp = items_list[i]
-                items_list[i] = items_list[i + 1]
-                items_list[i + 1] = temp
+                items_list[i], items_list[i + 1] = items_list[i + 1], items_list[i]
     return items_list
 
 
 my_list = [4, 2, 6, 5, 1, 3]
-my_list = bubble_sort(my_list)
+bubble_sort(my_list)
 print(my_list)
