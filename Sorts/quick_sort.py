2b3e958410eaad9bb3184fd2a77f24840eddeeb4
EBi <EBi.x2003x@gmail.com>
2024-09-17 19:21:41 +0330

    create quick_sort.py file and add basic functionality to quick_sort()

---
+++ Sorts/quick_sort.py
@@ -0,0 +1,15 @@
+def quick_sort(items_list):
+    pivot = items_list[0]
+    swap = 0
+    i = 1
+    while i < len(items_list):
+        if items_list[i] < pivot:
+            swap += 1
+            items_list[swap], items_list[i] = items_list[i], items_list[swap]
+        i += 1
+    items_list[0], items_list[swap] = items_list[swap], items_list[0]
+    return items_listi
+
+
+my_list = [4, 6, 1, 7, 3, 2, 5]
+print(quick_sort(my_list))
