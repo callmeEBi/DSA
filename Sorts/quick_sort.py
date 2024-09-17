ad45fe3eb271b86e9a3016082babcd08f6d9d17a
EBi <EBi.x2003x@gmail.com>
2024-09-17 19:24:42 +0330

    Fix typo in return statement of quick_sort function.

--- Sorts/quick_sort.py
+++ Sorts/quick_sort.py
@@ -8,7 +8,7 @@ def quick_sort(items_list):
             items_list[swap], items_list[i] = items_list[i], items_list[swap]
         i += 1
     items_list[0], items_list[swap] = items_list[swap], items_list[0]
-    return items_listi
+    return items_list
 
 
 my_list = [4, 6, 1, 7, 3, 2, 5]
